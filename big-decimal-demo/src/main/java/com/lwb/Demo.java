b06c870ceded500be54a8e826a69b12ad1fdeffc
刘维柏 <liuwb@youxuepai.com>
2019-12-27 18:06:05 +0800

    分布式锁

---
+++ big-decimal-demo/src/main/java/com/lwb/Demo.java
@@ -0,0 +1,16 @@
+package com.lwb;
+
+import org.junit.Test;
+
+/**
+ * @author liuweibo
+ * @date 2019/12/23
+ */
+public class Demo {
+
+
+    @Test
+    public void test01() {
+        System.out.println(0.35 + 0.35 + 0.35);
+    }
+}
--- redission-demo/src/main/java/com/lwb/redission/server/demo/BaseDemo.java
+++ redission-demo/src/main/java/com/lwb/redission/server/demo/BaseDemo.java
@@ -8,6 +8,15 @@ import org.redisson.config.TransportMode;
 
 public class BaseDemo {
 
-    protected static RedissonClient redissonClient = Redisson.create();
+    protected static RedissonClient redissonClient;
+
+    static {
+        Config config = new Config();
+        config.useSingleServer()
+            .setTimeout(1000000)
+//            .setPassword("123456")
+            .setAddress("redis://127.0.0.1:6379");
+        redissonClient = Redisson.create(config);
+    }
 
 }
