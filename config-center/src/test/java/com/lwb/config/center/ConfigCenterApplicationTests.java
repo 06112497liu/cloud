112fd29bdf7c278213f43bd57e305dff050513e6
06112497liu <804912547@qq.com>
2020-02-05 14:13:26 +0800

    【刘维柏】feign参数问题

---
+++ config-center/src/test/java/com/lwb/config/center/ConfigCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwb.config.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
